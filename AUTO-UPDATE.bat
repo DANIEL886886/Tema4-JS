1f2404a5170322341a6ded0daca66afaebe8da4d
DANIEL886886 <danielparaschiv886@gmail.com>
2024-05-02 00:43:32 +0300

    update-2024:05:02_00:43:32

--- AUTO-UPDATE.bat
+++ AUTO-UPDATE.bat
@@ -1,39 +1,55 @@
 cls
-color 6
-cls
+color 4
+cls]
 
+
+::------------------------------------------------------------------------------------------------------------------------
+MESAJ INTAMPINARE
+::------------------------------------------------------------------------------------------------------------------------
 echo Hello, %username%
 echo.
 echo.
 echo Incepe actualizarea ! ......................
 
-set mypath=%cd%
 
+::------------------------------------------------------------------------------------------------------------------------
+PENTRU A PRELUA AUTOMAT CALEA LOCALA UNDE SE AFLA FIZIC SCRIPTUL
+::------------------------------------------------------------------------------------------------------------------------
+set mypath=%cd%
 @echo %mypath%
 
+
+::------------------------------------------------------------------------------------------------------------------------
+COMENI GIT
+::------------------------------------------------------------------------------------------------------------------------
 cd %mypath%
-
 git checkout update
-
 git add .
-
 git status
 
+::------------------------------------------------------------------------------------------------------------------------
+COMENZI PENTRU A EXTRAGE DATA SI ORA PENTRU A LE FOLOSI IN DENUMIREA UPDATE-ULUI
+::------------------------------------------------------------------------------------------------------------------------
 @echo off
 for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
 set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
 set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
 
 set "datestamp=%YYYY%%MM%%DD%" & set "timestamp=%HH%%Min%%Sec%"
-set "fullstamp=%YYYY%-%MM%-%DD%_%HH%-%Min%-%Sec%"
+set "fullstamp=%YYYY%:%MM%:%DD%_%HH%:%Min%:%Sec%"
 echo datestamp: "%datestamp%"
 echo timestamp: "%timestamp%"
 echo fullstamp: "%fullstamp%"
 
-git commit -m "update-%datestamp%-%timestamp%"
 
+::------------------------------------------------------------------------------------------------------------------------
+COMENZI GIT PENTRU PUSH UPDATE
+::------------------------------------------------------------------------------------------------------------------------
+git commit -m "update-%fullstamp%"
 git push origin update
 
+
+::------------------------------------------------------------------------------------------------------------------------
 echo.
 echo.
 echo Actualizare finalizata cu success !
