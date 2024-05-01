c850b89bb683b5f7349788eb904479a4c10f3a92
DANIEL886886 <danielparaschiv886@gmail.com>
2024-05-02 00:31:59 +0300

    update-20240502-003159

---
+++ AUTO-UPDATE.bat
@@ -0,0 +1,25 @@
+@echo off
+
+git checkout update
+
+git add .
+
+git status
+
+@echo off
+for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
+set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
+set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
+
+set "datestamp=%YYYY%%MM%%DD%" & set "timestamp=%HH%%Min%%Sec%"
+set "fullstamp=%YYYY%-%MM%-%DD%_%HH%-%Min%-%Sec%"
+echo datestamp: "%datestamp%"
+echo timestamp: "%timestamp%"
+echo fullstamp: "%fullstamp%"
+
+git commit -m "update-%datestamp%-%timestamp%"
+
+git push origin update
+
+pause
+
