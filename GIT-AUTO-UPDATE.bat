a2d11370a21b05a61f07b45cb27abeab35b062d4
DANIEL886886 <danielparaschiv886@gmail.com>
2024-05-02 01:07:41 +0300

    update-2024-05-02_01:07:41

--- GIT-AUTO-UPDATE.bat
+++ GIT-AUTO-UPDATE.bat
@@ -35,9 +35,9 @@ echo.
 for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
 set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
 set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
-set "Data&Timp=%YYYY%%MM%%DD%" & set "timestamp=%HH%%Min%%Sec%"
-set "Data&Timp=%YYYY%:%MM%:%DD%_%HH%:%Min%:%Sec%"
-echo Data&Timp: "%Data&Timp%"
+set "datestamp=%YYYY%%MM%%DD%" & set "timestamp=%HH%%Min%%Sec%"
+set "fullstamp=%YYYY%-%MM%-%DD%_%HH%:%Min%:%Sec%"
+echo Data si Ora: "%fullstamp%"
 echo.
 ::------------------------------------------------------------------------------------------------------------------------
 ::COMENZI GIT PENTRU PUSH UPDATE
